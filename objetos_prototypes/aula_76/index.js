a978284d8f8d65adcd3d8809918b4981f6acca60
jonrato <jonatas_ferreira@usp.br>
2022-05-30 10:10:24 -0300

    prototype

---
+++ objetos_prototypes/aula_76/index.js
@@ -0,0 +1,16 @@
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+Produto.prototype.aumento = function(quantia){
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function(quantia){
+    this.preco -= quantia;
+};
+
+function Camiseta(nome, preco, cor){
+    Produto.call(this,nome,preco);
+}
+const camiseta = new Camiseta('Regata', 115, 'Preta');
+console.log(camiseta);
