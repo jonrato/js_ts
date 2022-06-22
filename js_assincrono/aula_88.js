aa797991d2d527d7c1dbc795fea2e477a2453529
jonrato <jonatas_ferreira@usp.br>
2022-06-22 11:29:05 -0300

    async await

---
+++ js_assincrono/aula_88.js
@@ -0,0 +1,29 @@
+function rand(min, max){
+    min *=1000;
+    max *=1000;
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+function esperaAi(msg, tempo){
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject(new Error('Erro'))
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo)
+    })
+}
+
+async function executa(){
+    try{
+        const fase1 = await esperaAi('fase 1', rand(1,3));
+        console.log(fase1);
+        const fase2 = await esperaAi('fase 2', rand(1,3));
+        console.log(fase2);
+        const fase3 = await esperaAi('fase 3', rand(1,3));
+        console.log(fase3);
+    }catch(e){
+        console.log(e)
+    }
+}
+
+executa();
